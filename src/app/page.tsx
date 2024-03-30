bd66a51ed12bd629b9cd13a37e7fb02d3b43c848
SimonBjorkberg <S.bjorkberg@gmail.com>
2024-03-30 19:16:12 +0100

    margins YEETED AND DELETED

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import Script from "next/script";
 import Info from "./components/Info";
 import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
 
 export default function Home() {
 
