b52123e27f3520b577a2309fbe20719a45798f3e
SimonBjorkberg <S.bjorkberg@gmail.com>
2024-03-30 18:58:56 +0100

    fixed icons 2.0

--- src/app/components/Info.tsx
+++ src/app/components/Info.tsx
@@ -8,8 +8,6 @@ import HtmlIcon from '../../../public/svgs/file-type-html.svg'
 import gitIcon from '../../../public/svgs/git.svg'
 import reactIcon from '../../../public/svgs/react.svg'
 import downArrow from '../../../public/svgs/arrow-down-1.svg'
-import githubIcon from '../../../public/svgs/github.svg'
-import linkedinIcon from '../../../public/svgs/linkedin.svg'
 import { ReactTyped } from "react-typed";
 import Link from "next/link";
 
@@ -32,7 +30,7 @@ export default function Info() {
                             </Link>
                         </li>
                         <Link href={"https://github.com/SimonBjorkberg?tab=repositories"} rel="noopener noreferrer" target="_blank">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="1 1 21 22" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="hover:stroke-[#FCCA46] stroke-[#FE7F2D] transition-all duration-200"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="hover:stroke-[#FCCA46] stroke-[#FE7F2D] transition-all duration-200"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
                         </Link>
                     </ul>
                 </div>
