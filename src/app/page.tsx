d38c1b79183a2fed64640e635b5232dc99fc89e7
SimonBjorkberg <S.bjorkberg@gmail.com>
2024-03-30 19:43:36 +0100

    experimenting with margins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Navbar from "./components/Navbar";
 export default function Home() {
 
   return (
-    <main className={`pixelify-sans-font overflow-x-hidden h-fit`}>
+    <main className={`pixelify-sans-font overflow-x-hidden`}>
       <Navbar />
         <Info />
         <Script src="/js/index.js" />
